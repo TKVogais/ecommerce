0c85de6396c56fc8314b7beabf20c1a8f220b844
TKVogais <jgparanagua@gmail.com>
2022-05-14 18:03:01 -0300

    Versão 3.0.2

--- src/controllers/actions/ControllerPerfilButton.js
+++ src/controllers/actions/ControllerPerfilButton.js
@@ -3,6 +3,6 @@ const BtnCar = document.getElementById('carrinho-perfil')
 if (BtnCar) {
     BtnCar.addEventListener("click", () => {
         const Key = localStorage.getItem("key")
-        window.location.href = `http://localhost:8081/carrinho/${Key}`
+        window.location.href = `http://www.otakushopp.com/carrinho/${Key}`
     })
 }
